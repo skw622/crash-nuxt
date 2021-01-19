e5a03304a98747b2cffd6d1b865193f29c39d069
skw622 <skw622vlato@gmail.com>
2021-01-19 07:31:06 -0500

    skw

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>About Crach</h1>
+    <p>This is an app that displays corny dad</p>
+  </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'About The App',
+            meat: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'besjadsfa'
+                }
+            ]
+        }
+    }
+};
+</script>
+<style>
+</style>
